ac0f9bdd4ac7cff92c448c05c58e7adf6ca61054
Giulianno Zilli <gzilli@hotmail.com>
2020-12-02 22:07:25 -0300

    adad

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-console.log(firebase);
+function registrarusuario() {
+  //console.log("estoy dentro de la funcion");
+
+  var email = document.getElementById("email").value;
+  var password = document.getElementById("pasw").value;
+
+  firebase
+    .auth()
+    .createUserWithEmailAndPassword(email, password)
+    .then((user) => {
+      // Signed in
+      // ...
+    })
+    .catch((error) => {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // ..
+    });
+}
